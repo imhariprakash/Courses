b998278060d851d79fa5b5e9d56875c6e3abcd85
HARIPRAKASH1805 <58242393+HARIPRAKASH1805@users.noreply.github.com>
2021-07-15 18:27:18 +0530

    Add files via upload

---
+++ intro_to_cs_london_univ/course1/sleuth game/case 201/stage 2/sketch.js
@@ -0,0 +1,35 @@
+/*
+201 - The case of Judge Hopper
+Stage 2 - Hopper’s House
+
+Officer: 3698339
+CaseNum: 201-1-73112251-3698339
+
+On entering the house you’re immediately confronted by a trail of blood spatters leading up to the master bedroom. It’s clear that Judge Hopper was abducted from here. What’s more, the clues splayed across the bed sheets indicate a private life a little more exciting than a district judge would admit in public. Somehow these clues connect, you just need to join them together.
+
+Use the triangle function to join the blood spatters on the clues and make the connection.
+
+*/
+
+var img;
+
+function preload()
+{
+    img = loadImage('scene.png');
+}
+
+function setup()
+{
+    createCanvas(img.width,img.height);
+}
+
+function draw()
+{
+
+    image(img,0,0);
+    stroke(255, 0, 0);
+    strokeWeight(3);
+    noFill();
+
+    triangle(152,376,597,452,990,330);
+}
