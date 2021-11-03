87f8ef0f0ac007d733ac5f0b058fe0870048d86d
HARIPRAKASH1805 <58242393+HARIPRAKASH1805@users.noreply.github.com>
2021-11-03 19:08:00 +0530

    Create use_arg_from_cmd.c

---
+++ C language/programs/Dartmouth_IMTX/Modularity/use_arg_from_cmd.c
@@ -0,0 +1,29 @@
+Code discussed in this video:
+
+#include <stdio.h>
+#include <stdlib.h> // for atoi and atof
+#include <string.h> // for strcmp
+
+int main(int argc, char *argv[]) {
+    int i, compare, wholeNumber;
+    double decimalNumber;
+    
+    printf("I have %d arguments from the command line: \n", argc);
+    for (i = 0; i<argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+    
+    if (argc == 4) {
+        compare = strcmp(argv[1], "-e");
+        if (compare ==0) {
+            printf("argv[1] equals -e \n");
+        } else {
+            printf("argv[1] does not equal -e \n");
+        }
+        wholeNumber = atoi(argv[2]);
+        printf("%s as an integer is %d.\n", argv[2], wholeNumber);
+        decimalNumber = atof(argv[3]);
+        printf("%s as a double is %lf.\n", argv[3], decimalNumber);
+    }
+    return 0;
+}
