0d386b145c782a4cd2ea42edb0eba1a49fdcf53f
HARIPRAKASH1805 <58242393+HARIPRAKASH1805@users.noreply.github.com>
2021-11-03 13:18:44 +0530

    Update program.c

--- C language/programs/Dartmouth_IMTX/Modularity/program.c
+++ C language/programs/Dartmouth_IMTX/Modularity/program.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include "weather.h"
+#include "weatherstats.h"
 
 int main(void) {
-	double temperatures[7] = {6.9, 12.3, 9.0, 5.3, 9.8, 1.8, 0.3};
-	printAverageTemp(temperatures, 7);
+	double temperatures[7] = {6.9, 12.3, 9.0, 15.3, 9.8, 1.8, 0.3};
+	double average = averageTemp(temperatures, 7);
+	double max = maxTemp(temperatures, 7); 
+	printf("Average 7-day temp: %.2lf\n", average);
+	printf("Highest temp: %.2lf\n", max);
+	printf("Lowest temperature: %.2lf\n", minTemp(temperatures,7));
 	return 0;
 }
 
