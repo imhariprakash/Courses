8b6ba5d283b868acab497e6ae368d91dae0f0b53
HARIPRAKASH1805 <58242393+HARIPRAKASH1805@users.noreply.github.com>
2022-02-05 17:15:57 +0530

    keyword-string-methods

---
+++ Java/websites/useCodePointAt.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+
+class useCodePointAt{
+    public static void main(String[] args){
+        String str = "Ahello";
+        int a = str.codePointAt(0);
+        System.out.println((int)'B');
+    }
+}
