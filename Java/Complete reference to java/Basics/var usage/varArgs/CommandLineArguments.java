8514ced410b1525e257f86788e32320f4e2b463f
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-03-18 21:32:35 +0530

    var

---
+++ Java/Complete reference to java/Basics/var usage/varArgs/CommandLineArguments.java
@@ -0,0 +1,24 @@
+class CommandLineArguments{
+    public static void main(String[] args){
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
+
+/*
+
+PS C:\Users\S HARIPRAKASH\Desktop\String> java CommandLineArguments hello java you are so beautiful 100 marks out of 100
+hello
+java
+you
+are
+so
+beautiful
+100
+marks
+out
+of
+100
+
+*/
