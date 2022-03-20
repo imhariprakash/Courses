10729381e7ad44bad98e2ca909a68a6237050857
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-03-20 09:11:52 +0530

    middle

---
+++ C language/DS/LinkedList/SlowAndFast.c
@@ -0,0 +1,86 @@
+#include<Stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node* AddLast(struct node * head, int value){
+    struct node * temp = malloc(sizeof(struct node));
+    temp -> data = value;
+    temp -> next = NULL;
+    if(head == NULL){
+        return temp;
+    }
+    else{
+        struct node *traverse = head;
+        while(traverse -> next != NULL){
+            traverse = traverse->next;
+        }
+        traverse -> next = temp;
+        return(head);
+    }
+}
+
+
+void printList(struct node *head){
+    struct node *temp = head;
+    while(temp != NULL){
+        printf("%d ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("\n");
+}
+
+void Deallocate(struct node *head){
+    struct node *temp = head;
+    while(temp != NULL){
+        head = temp -> next;
+        free(temp);
+        temp = head;
+    }
+}
+
+void getMiddle(struct node *head){
+    if(head == NULL){
+        return;
+    }
+    struct node *slow = head;
+    struct node *fast = head;
+    while(fast != NULL){
+        if(fast -> next == NULL){
+            break;
+        }
+        else if(fast -> next -> next == NULL){
+            break;
+        }
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    printf("%d\n", slow -> data);
+}
+
+int main(){
+    int choice, value;
+    struct node *head = NULL;
+
+    printf("1. Add 2. Print 3. exit\n");
+
+    do{
+        printf("Enter choice : ");
+        scanf("%d", &choice);
+        if(choice == 1){
+            printf("Enter value : ");
+            scanf("%d", &value);
+            head = AddLast(head, value);
+        }
+        else if(choice == 2){
+            printList(head);
+        }
+        else if(choice == 3){
+            break;
+        }
+    }while(1);
+    getMiddle(head);
+}
