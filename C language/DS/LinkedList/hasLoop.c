423e817db61de2cde4e2112661951a44b3c19d47
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-03-20 15:53:42 +0530

    hasLoop

--- C language/DS/LinkedList/hasLoop.c
+++ C language/DS/LinkedList/hasLoop.c
@@ -10,7 +10,7 @@ struct node{
 int hasLoop(struct node *head){
     struct node *slow = head;
     struct node *fast = head;
-    while(fast != NULL && fast -> next != NULL){
+    while(start != NULL && fast != NULL && fast -> next != NULL){
         slow = slow -> next;
         fast = fast -> next -> next;
         if(slow == fast){
