5a249ef053ddfaa72ecb51dbd5bcd3c8e5f712ce
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-03-27 13:45:05 +0530

    pattern4

--- zoho/Java/Patterns/pattern5/pattern4.java
+++ zoho/Java/Patterns/pattern5/pattern4.java
@@ -1,3 +1,5 @@
+/*   working only for odd - for even not as intented. idea : skip 1 middle for even - working 
+
 import java.util.Scanner;
 class pattern4{
     public static void main(String[] args){
@@ -54,4 +56,63 @@ g                       s
   e                   k
 g                       s
 
+*/
+
+*/
+    
+    
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = getLength(str);
+        for(int i = 0; i < n; i++){
+            if(n % 2 == 0 && i == n / 2)
+                continue;
+            for(int j = 0; j < n; j++){
+                if(i == j || i + j == n -1){
+                    System.out.print(str.charAt(j) + " ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int getLength(String str){
+        int count = 0;
+        try{
+            while(true){
+                str.charAt(count++);
+            }
+        }catch(Exception e){
+            return count - 1;
+        }
+    }
+}
+
+/*----------------------------------------------------------------
+12345
+1       5 
+  2   4   
+    3     
+  2   4   
+1       5 
+geeksforgeeks
+g                       s 
+  e                   k   
+    e               e     
+      k           e       
+        s       g
+          f   r
+            o
+          f   r
+        s       g
+      k           e
+    e               e
+  e                   k
+g                       s
 */
