e6e5ff834e8a07ac8f72e405a9d450c7e8725893
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-04-03 22:21:57 +0530

    date time
    use documentation for this

---
+++ SQL/Mysql/10. Revisiting Data Types/datetime.sql
@@ -0,0 +1,23 @@
+/*
+
+CREATE DATABASE people_db;
+CREATE TABLE people_db.people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+
+INSERT INTO people_db.people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+
+INSERT INTO people_db.people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+
+
+SELECT * FROM people_db.people;
+
+SELECT CURDATE();
+
+SELECT CURTIME();
+
+
+SELECT NOW();
+
+
+*/
