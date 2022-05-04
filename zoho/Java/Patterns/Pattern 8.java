978a298f917af2f95d1f20ae34140391252a60b3
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-05-04 23:02:43 +0530

    pattern 8
    7
    0 0 0 0 0 0 0 0 0 0 0 
    1 0 0 0 0 0 0 0 0 0 1
    2 1 0 0 0 0 0 0 0 1 2 
    3 2 1 0 0 0 0 0 1 2 3
    4 3 2 1 0 0 0 1 2 3 4
    5 4 3 2 1 0 1 2 3 4 5
    6 5 4 3 2 1 2 3 4 5 6

--- zoho/Java/Patterns/Pattern 8.java
+++ zoho/Java/Patterns/Pattern 8.java
@@ -1,22 +1,65 @@
 import java.util.Scanner;
  
-class Pattern{
+class myClass{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter n : ");
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            for(int j = i; j > 1; j--){
-                System.out.print(j + " ");
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(i - j + " ");
             }
-            for(int j = 1; j <= n - i + 1; j++){
+            for(int j = 0; j < (2 * (n - 1) - 1) - 2 * i; j++){
+                System.out.print("0 ");
+            }
+            for(int j = 1; j <= i; j++){
+                if(i == n - 1 && j == 1){
+                    continue;
+                }
                 System.out.print(j + " ");
             }
             System.out.println();
         }
 
         sc.close();
-
     }
 }
+
+
+/*
+
+4
+0 0 0 0 0 
+1 0 0 0 1 
+2 1 0 1 2 
+3 2 1 2 3 
+
+
+5
+0 0 0 0 0 0 0 
+1 0 0 0 0 0 1
+2 1 0 0 0 1 2
+3 2 1 0 1 2 3
+4 3 2 1 2 3 4 
+
+
+6
+0 0 0 0 0 0 0 0 0 
+1 0 0 0 0 0 0 0 1
+2 1 0 0 0 0 0 1 2
+3 2 1 0 0 0 1 2 3
+4 3 2 1 0 1 2 3 4
+5 4 3 2 1 2 3 4 5
+
+
+7
+0 0 0 0 0 0 0 0 0 0 0 
+1 0 0 0 0 0 0 0 0 0 1
+2 1 0 0 0 0 0 0 0 1 2 
+3 2 1 0 0 0 0 0 1 2 3
+4 3 2 1 0 0 0 1 2 3 4
+5 4 3 2 1 0 1 2 3 4 5
+6 5 4 3 2 1 2 3 4 5 6
+
+
+*/
