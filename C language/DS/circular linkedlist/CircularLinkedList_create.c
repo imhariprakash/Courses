25ceb8cb43d59eabb4b6fb774d2d602c0d56763c
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-05-06 17:44:18 +0530

    Add files via upload

---
+++ C language/DS/circular linkedlist/CircularLinkedList_create.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+int main(){
+    struct node *elem1, *elem2, *elem3, *head, *temp, *tail;
+    elem1 = malloc(sizeof(struct node));
+    elem2 = malloc(sizeof(struct node));
+    elem3 = malloc(sizeof(struct node));
+
+    head = elem1;
+    tail = elem3;
+
+    elem1->data = 1;
+    elem1->prev = elem3;
+    elem1->next = elem2;
+
+    elem2->data = 2;
+    elem2->prev = elem1;
+    elem2->next = elem3;
+
+    elem3->data = 3;
+    elem3->prev = elem2;
+    elem3->next = elem1;
+
+    temp = head;
+
+    do{
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }while(temp != head);
+
+    printf("\n");
+
+    temp = tail;
+
+    do{
+        printf("%d ", temp->data);
+        temp = temp->prev;                      // reverse print
+    }while(temp != tail);
+
+    printf("\n");
+
+    return 0;
+}
