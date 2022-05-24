94d7a2549ff4144086c3f1b4b986787d8e9f5f13
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2022-05-24 20:48:42 +0530

    file open

---
+++ python/Simple Programs/file open.py
@@ -0,0 +1,16 @@
+# try:
+#     file = open(r'C:\Users\S HARIPRAKASH\Desktop\sample.txt', "r")
+# except:
+#     print("File not found")
+#     exit()
+# count = 0
+# for line in file:
+#     print(line, end='')
+#     count += 1
+# print(count)
+
+
+dict = {'csev' : 1, 'cwen' : 2, 'csev' : 3, 'zqian' : 4, 'cwen' : 5} 
+
+
+
